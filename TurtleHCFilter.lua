a7fc20e8b0c952fa9446191cea8ce725e52e7833
David Greene <david@trumpetx.com>
2023-10-17 09:40:44 -0500

    Adding debug mode and level filtering of WTS/B/T LFM/G style messages with ##+ hints

--- TurtleHCFilter.lua
+++ TurtleHCFilter.lua
@@ -1,13 +1,47 @@
 if HCFFrame == nil then HCFFrame = 1 end
 if HCFPrefix == nil then HCFPrefix = "HC" end
 if HCFColour == nil then HCFColour = "e6cd80" end
+if HCFLevelFilter == nil then HCFLevelFilter = true end
+if HCFDebug == nil then HCFDebug = false end
+
+local gfind = string.gmatch or string.gfind
+local HC_LEVEL_RANGE = 5
 
 TurtleHCFilter_ChatFrame_OnEvent = ChatFrame_OnEvent
 HCFSpam = ''
 
+function FindAny(str, ...)
+	for _,v in ipairs(arg) do
+		local _start, _end = string.find(str, v)
+		if _start ~= nil then
+			return _start
+		end
+	end
+	return nil
+end
+
+function FilterOut(chat)
+	if HCFLevelFilter then
+		local marker = FindAny(string.lower(arg1), "wts", "wtb", "wtt", "lf%d*m", "lfg")
+		if marker ~= nil then
+			local levelStart, levelEnd = string.find(arg1, "%d+ *+")
+			if levelStart ~= nil then
+				local plusLevel = tonumber(string.sub(arg1, levelStart, levelEnd-1))
+				local myLevel = UnitLevel("player")
+				if plusLevel < myLevel - HC_LEVEL_RANGE or plusLevel > myLevel + HC_LEVEL_RANGE then
+					Debug("- " .. arg1)
+					HCFSpam = arg1
+					return true
+				end
+			end
+		end
+	end
+	return false
+end
+
 function ChatFrame_OnEvent(event)
 	if (event == "CHAT_MSG_HARDCORE") then
-		if HCFSpam == arg1 then
+		if HCFSpam == arg1 or FilterOut(arg1) then
 			return false
 		end
 		local prefix
@@ -51,6 +85,12 @@ function Message(message)
 	DEFAULT_CHAT_FRAME:AddMessage("|cffbe5effHCF|r "..message)
 end
 
+function Debug(message)
+	if HCFDebug then
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbe5effHCF|cffffff00 "..message)
+	end
+end
+
 function SetFrame(frameString)
 	local frame = tonumber(frameString)
 	if frame == nil then
@@ -63,7 +103,6 @@ end
 
 SLASH_TurtleHCFilter1, SLASH_TurtleHCFilter2 = "/HCF", "/HCFilter"
 SlashCmdList["TurtleHCFilter"] = function(message)
-	local gfind = string.gmatch or string.gfind
 	local frame = tonumber(message)
 	if frame == nil then
 		local commandlist = { }
@@ -88,6 +127,17 @@ SlashCmdList["TurtleHCFilter"] = function(message)
 			else
 				Message("Channel prefix set to: "..HCFPrefix)
 			end
+		elseif commandlist[1] == "info" then
+			Message("Debug mode " .. (HCFDebug and "enabled" or "disabled"))
+			Message("Level filtering " .. (HCFLevelFilter and "enabled" or "disabled"))
+			Message("Channel prefix set to " .. (HCFPrefix and HCFPrefix or "nothing"))
+			Message("Channel frame set to " .. HCFFrame)
+		elseif commandlist[1] == "debug" then
+			HCFDebug = not HCFDebug
+			Message("Debug mode " .. (HCFDebug and "enabled" or "disabled"))
+		elseif commandlist[1] == "levelfilter" then
+			HCFLevelFilter = not HCFLevelFilter
+			Message("Level filtering " .. (HCFLevelFilter and "enabled" or "disabled"))
 		elseif commandlist[1] == "colour" or commandlist[1] == "color" then
 			if commandlist[2] == nil then
 				Message("Channel prefix set to the default: |cffe6cd80e6cd80")
